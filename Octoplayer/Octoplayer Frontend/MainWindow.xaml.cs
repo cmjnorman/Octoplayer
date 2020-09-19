8f7c17fc785324ace5f20d8d0aed25133dfcc30a
cmjnorman <cmjnorman@gmail.com>
2020-09-19 19:42:13 +0100

    Add basic file selection and play/pause functionality

--- Octoplayer/Octoplayer Frontend/MainWindow.xaml.cs
+++ Octoplayer/Octoplayer Frontend/MainWindow.xaml.cs
@@ -1,28 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+
 
 namespace Octoplayer_Frontend
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer player = new MediaPlayer();
+        private bool isPlaying = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Audio files|*.mp3; *.flac; *.wav |All files (*.*)|*.*";
+            if(dialog.ShowDialog() == true)
+            {
+                Pause();
+                txtFilePath.Text = dialog.FileName;
+                player.Open(new Uri(dialog.FileName));
+            }
+        }
+
+        private void btnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                Pause();
+            }
+            else
+            {
+                Play();
+            }
+        }
+
+        private void Play()
+        {
+            player.Play();
+            btnPlayPause.Content = FindResource("Pause");
+            isPlaying = true;
+        }
+
+        private void Pause()
+        {
+            player.Pause();
+            btnPlayPause.Content = FindResource("Play");
+            isPlaying = false;
         }
     }
 }
