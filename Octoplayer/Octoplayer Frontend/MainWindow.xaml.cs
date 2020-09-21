0f5ba1ee6b1cff48a264a2edce54f68e82eeaf45
cmjnorman <cmjnorman@gmail.com>
2020-09-21 17:25:38 +0100

    Add text fields to display track title, artist(s), and album, and an image to display the album artwork.

--- Octoplayer/Octoplayer Frontend/MainWindow.xaml.cs
+++ Octoplayer/Octoplayer Frontend/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Microsoft.Win32;
+using TagLib;
 
 
 namespace Octoplayer_Frontend
@@ -10,6 +15,7 @@ namespace Octoplayer_Frontend
     {
         private MediaPlayer player = new MediaPlayer();
         private bool isPlaying = false;
+        private TagLib.File track;
         public MainWindow()
         {
             InitializeComponent();
@@ -18,12 +24,33 @@ namespace Octoplayer_Frontend
         private void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Audio files|*.mp3; *.flac; *.wav |All files (*.*)|*.*";
+            dialog.Filter = "Audio files|*.mp3; *.flac; *.wav|All files|*.*";
             if(dialog.ShowDialog() == true)
             {
                 Pause();
                 txtFilePath.Text = dialog.FileName;
+                track = TagLib.File.Create(dialog.FileName);
                 player.Open(new Uri(dialog.FileName));
+
+                lblTrackTitle.Content = track.Tag.Title;
+                var artists = new StringBuilder();
+                artists.Append(track.Tag.Performers[0]);
+                foreach (var artist in track.Tag.Performers.Skip(1))
+                {
+                    artists.Append(artist);
+                }
+                lblArtists.Content = artists;
+                lblAlbum.Content = track.Tag.Album;
+
+                MemoryStream ms = new MemoryStream(track.Tag.Pictures[0].Data.Data);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+
+                imgAlbumArt.Source = bitmap;
             }
         }
 
